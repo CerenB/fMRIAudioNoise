2c3d9ebfc994c4d5197bb87b05d461747a1d5d05
Ceren <battal.ceren@gmail.com>
2020-12-03 15:52:40 +0100

    first commit

---
+++ getFFT.m
@@ -0,0 +1,22 @@
+
+filePath = '~/Desktop/fmri_acq3_macMic.mp3';
+
+[s,fs] = audioread(filePath);
+
+%documentation needed!
+
+N = length(s);
+hN = floor(N/2)+1;
+freq = [0:hN-1]/N*fs;
+
+w = hann(N);
+s = w .* s;
+
+mX = abs(fft(s));
+mX(1) = 0;
+
+figure('name',filePath)
+plot(freq,mX(1:hN))
+xlim([0,2000])
+
+datacursormode on
